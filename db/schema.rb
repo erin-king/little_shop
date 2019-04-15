69a3cf2ae49b38f792e041dc22b7ad772b4df1e2
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-15 08:52:04 -0600

    Add active column for coupons table, completed test and view for enable/disable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190415011909) do
+ActiveRecord::Schema.define(version: 20190415123137) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20190415011909) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: true
     t.index ["user_id"], name: "index_coupons_on_user_id"
   end
 
--- spec/features/merchants/coupon_show_spec.rb
+++ spec/features/merchants/coupon_show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "As a merchant" do
   before :each do
     @merchant = create(:merchant)
     @c1 = @merchant.coupons.create(code: "10OFF", discount: 0.10)
+    @c2 = @merchant.coupons.create(code: "10OFF", discount: 0.10, active: false)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@merchant)
   end
 
@@ -17,21 +18,45 @@ RSpec.describe "As a merchant" do
     end
 
     it "has a Add New Coupon link" do
+      visit dashboard_coupon_path(@c1)
+
+      expect(current_path).to eq(dashboard_coupon_path(@c1))
+      expect(page).to have_content(@c1.code)
+      expect(page).to have_link("Add New Coupon")
     end
 
     it "has an Edit Coupon link" do
+      visit dashboard_coupon_path(@c1)
 
+      expect(current_path).to eq(dashboard_coupon_path(@c1))
+      expect(page).to have_content(@c1.code)
+      expect(page).to have_link("Edit Coupon")
     end
 
     it "has a Delete Coupon link" do
+      visit dashboard_coupon_path(@c1)
 
-    end
-
-    it "has an Enable Coupon link" do
+      expect(current_path).to eq(dashboard_coupon_path(@c1))
+      expect(page).to have_content(@c1.code)
+      expect(page).to have_link("Edit Coupon")
     end
 
     it "has a Disable Coupon link" do
+      visit dashboard_coupon_path(@c1)
 
+      expect(current_path).to eq(dashboard_coupon_path(@c1))
+      expect(page).to have_content(@c1.code)
+      expect(page).to have_link("Disable Coupon")
+      expect(page).to_not have_link("Enable Coupon")
+    end
+
+    it "has an Enable Coupon link" do
+      visit dashboard_coupon_path(@c2)
+
+      expect(current_path).to eq(dashboard_coupon_path(@c2))
+      expect(page).to have_content(@c2.code)
+      expect(page).to have_link("Enable Coupon")
+      expect(page).to_not have_link("Disable Coupon")
     end
   end
 end
