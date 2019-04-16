70bd4ffdbe0fffac2001ffa8b711e9accaaa73d7
Erin King <44042415+erin-king@users.noreply.github.com>
2019-04-16 07:02:12 -0600

    Add feature test cart/coupon_spec to test coupon code field

---
+++ spec/features/cart/coupon_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe "using coupons in the cart" do
+  before :each do
+    @user_1 = create(:user)
+    @merchant_1 = create(:merchant)
+    @merchant_2 = create(:merchant)
+    @item_1 = create(:item, user: @merchant_1, inventory: 3)
+    @item_2 = create(:item, user: @merchant_2)
+    @item_3 = create(:item, user: @merchant_2)
+    @coupon_10 = @merchant_1.coupons.create(code: "10OFF", discount: 0.1)
+  end
+
+  describe "as a visitor or regular user when I visit my cart" do
+    it "I can see a coupon code field and submit button" do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
+      # visit login_path
+      # fill_in :email, with: @user_1.email
+      # fill_in :password, with: @user_1.password
+      # click_button "Log in"
+      visit cart_path
+      visit item_path(@item_1)
+      click_on "Add to Cart"
+      visit item_path(@item_2)
+      click_on "Add to Cart"
+      visit item_path(@item_3)
+      click_on "Add to Cart"
+      visit item_path(@item_3)
+      click_on "Add to Cart"
+      
+      fill_in "Coupon Code", with: "10OFF"
+      click_button "Submit Code"
+
+      expect(current_path).to eq(cart_path)
+      expect(page).to have_content("10OFF")
+    end
+  end
+end
